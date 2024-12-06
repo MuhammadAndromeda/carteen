64491f7cf614f383d6ef1f01b5b71ac0de6c1bd0
andromeda <q.a.r.h.hrp0204@gmail.com>
2024-12-06 20:48:35 +0700

    forth-commit

--- create.php
+++ create.php
@@ -30,12 +30,63 @@ if(isset($_GET['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Your Product</title>
     <link rel="stylesheet" href="css/create.css">
+    <style>
+        nav a{
+            width: 15vw;
+            padding: 10px 0;
+            color: white;
+            text-decoration: none;
+            text-align: center;
+            font-size: 1rem;
+            font-weight: 500;
+            border: 3px solid white;
+            border-radius: 30px;
+            transition: all 0.3s ease-in-out;
+        }
+
+        nav a:hover, nav a:focus{
+            color: #1b5f1b;
+            background-color: white;
+            border: 3px solid white;
+            box-shadow: 0px 0px 10px 1px white;
+        }
+
+        nav form{
+            width: max-content;
+            height: max-content;
+            padding: 10px;
+            box-shadow: none;
+        }
+
+        nav form button{
+            width: 15vw;
+            padding: 10px 0;
+            color: white;
+            background-color: transparent;
+            border: 2px solid white;
+            border-radius: 30px;
+            text-decoration: none;
+            text-align: center;
+            font-size: 1rem;
+            font-weight: 600;
+            cursor: pointer;
+            transition: all 0.3s ease-in-out;
+        }
+
+        nav form button:hover, .navbar form button:focus{
+            color: #1b5f1b;
+            background-color: white;
+            box-shadow: 0px 0px 10px 1px white;
+        }
+    </style>
 </head>
 <body>
     <nav>
         <a href="adminmarket.php">All Products</a>
         <h1>CARTEEN MARKETPLACE</h1>
-        <a href="edit.php">Edit Product</a>
+        <form action="auth/logout.php" method="post">
+            <button type="submit">Log Out</button>
+        </form>
     </nav>
     <section class="create">
         <form action="create.php" method="get">
